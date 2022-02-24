11eb2f11084232a1f9e3f05d82037d4c11eddd1d
Hovanella <howansky.timofei@yandex.ru>
2022-02-24 11:24:43 +0300

    Dima Loh

---
+++ Laba1Remastered/Laba1Remastered/MatrixControler.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Laba1Remastered
+{
+    public static class MatrixControler
+    {
+        public static void InitializeMatrix(CMatrix matrix)
+        {
+            var randomizer = new Random();
+            for (var i = 0; i < matrix.NumberOfRows; i++)
+            {
+                for (var i1 = 0; i1 < matrix.NumberOfColumns; i1++)
+                {
+                    matrix.Content[i, i1] = randomizer.Next(1, 10);
+                }
+            }
+        }
+        
+        
+    }
+}
